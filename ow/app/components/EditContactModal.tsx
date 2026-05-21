4c78455bf734ef40d23b6ace1c0d330b05c3a215
Bill Douglas <billdouglas@Bills-2024-MacBook-Pro.local>
2026-05-20 19:45:48 -0600

    auto: file changes 2026-05-20 19:45:48

--- ow/app/components/EditContactModal.tsx
+++ ow/app/components/EditContactModal.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useRouter } from "next/navigation";
+import { OrganizationPicker } from "./OrganizationPicker";
+import type { PickerOption } from "./InlineCreatePicker";
 
 interface EditContactModalProps {
   isOpen: boolean;
