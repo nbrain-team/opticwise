717e2b4c28d11c1d49ee6007f3eb2747dcdc16af
Bill Douglas <bill@opticwise.com>
2026-05-26 10:18:11 -0600

    auto: file changes 2026-05-26 10:18:11

--- ow/app/social/compose/page.tsx
+++ ow/app/social/compose/page.tsx
@@ -9,6 +9,7 @@ import {
   Linkedin, Instagram, Loader2, X, MessageSquareText, Clock,
   FileText, Wand2, Shield, ChevronDown,
 } from 'lucide-react';
+import SocialPostPreview from './components/SocialPostPreview';
 
 interface Account {
   id: string;
